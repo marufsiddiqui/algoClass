56658abbc65fcc9e02439e46b7113cfb157ff0bf
Maruf Siddiqui <maruf.siddiqui@shore.com>
2017-07-23 15:52:46 +0200

    Complete the factorial challenge

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -3,3 +3,21 @@ Implement factorial.
 
 factorial(5) => 5*4*3*2*1 => 120
 */
+
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+function factorial2(n) {
+    let result = 1;
+    for(var i = 2; i <= n; i++) {
+        result *= i
+    }
+    return result;
+}
+
+console.log(factorial(5))
+console.log(factorial2(5))
